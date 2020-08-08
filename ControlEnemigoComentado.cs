28e8e2472a606c881aec71ccc6d65befeec62c93
Alejandra <67431364+Just-Lex@users.noreply.github.com>
2020-08-08 00:10:10 -0500

    Add files via upload

---
+++ ControlEnemigoComentado.cs
@@ -0,0 +1,58 @@
+﻿//Nombre del desarrollador: Alejandra Bravo A.
+//Asignatura: Estructura de datos
+//Descripción del uso de este código:
+ //Código para respawn de enemigos, contador y cantidad
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlEnemigos : MonoBehaviour
+{
+
+    public static int contadorEnemigos;
+
+    public GameObject objEnemigo;
+
+    public Transform[] spawnPoint;
+
+    public int infoCantidadEnemigos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contadorEnemigos = 0;
+        spawnPoint = new Transform[transform.childCount];
+        for (int i = 0; i < spawnPoint.Length; i++)
+        {
+            spawnPoint[i] = transform.GetChild(i);
+        }
+        Invoke("spawn",6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        infoCantidadEnemigos = contadorEnemigos;
+
+        infoCantidadEnemigos = contadorEnemigos;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+          
+           
+        }
+    }
+    void spawn()
+    {
+        foreach (var p in spawnPoint)
+        {
+            GameObject EnemigoPos = Instantiate(objEnemigo) as GameObject;
+
+            EnemigoPos.transform.position = p.position;
+            if (contadorEnemigos >= 5)
+            {
+                Destroy(EnemigoPos);
+            }
+        }
+    }
+}
---
+++ dañoEnemigoComentado.cs
@@ -0,0 +1,18 @@
+﻿//Nombre del desarrollador: Alejandra Bravo A.
+//Asignatura: Estructura de datos
+//Descripción del uso de este código:
+//Código daño por cólisiones con enmigos 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Se registra la colision
+public class dañoEnemigo : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(this.gameObject);
+    }
+}
